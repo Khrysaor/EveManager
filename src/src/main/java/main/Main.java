79e2fc7cbc4d2b3811969a8cc8d0c4a577324f31
Khrysaor <83225884+Khrysaor@users.noreply.github.com>
2021-04-27 08:38:07 -0400

    Update Main.java

--- src/src/main/java/main/Main.java
+++ src/src/main/java/main/Main.java
@@ -4,29 +4,36 @@ import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
+import Marketer;
 
 public class Main {
 
 	public static void main(String[] args) {
-        String token = "";
-
-        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
-
-        api.addMessageCreateListener(new MessageCreateListener() {
+		String token = "";
+		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+		
+		api.addMessageCreateListener(new MessageCreateListener() {
 			public void onMessageCreate(MessageCreateEvent event) {
 				String message = event.getMessageContent();
 				String[] messageArray = message.split(" ", 0);
-				
-			    if (messageArray[0].equalsIgnoreCase("!ping")) {
-			        event.getChannel().sendMessage("Pong!");
-			    } else if (messageArray[0].equalsIgnoreCase("!price")) {
-			    	event.getChannel().sendMessage("Pricing for " + messageArray[1]);
-			    	
-			    }
+				if (messageArray[0].equalsIgnoreCase("!ping")) {
+					event.getChannel().sendMessage("Pong!");
+				} else if (messageArray[0].equalsIgnoreCase("!price")) {
+					event.getChannel().sendMessage("Pricing for " + messageArray[1]);
+					String Values = Marketer.ReadJSON(messageArray[1], messageArray[2]);
+					if (Values = "") {
+						event.getChannel().sendMessage('Make sure to enter the proper values! \r\n ');
+					} else {
+						String[] valuesArray = Values.split(">", 0);
+						String[] valuesArrayArray = null;
+						for (i = 0; i < valuesArray.length; i++) {
+							String[] valuesArrayArray += valuesArray[i].split("<", 0);
+						}
+					}
+				}
 			}
 		});
-        
-        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+		
+		System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
 	}
-
 }
